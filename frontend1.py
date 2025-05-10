4af816df680238d9656e93edf0a35640ab28f20a
DataStuff-bit <sharibansh@gmail.com>
2025-05-10 23:35:15 +0530

    frontend1.py

--- frontend1.py
+++ frontend1.py
@@ -2008,8 +2008,9 @@ def main():
     elif selected_page == "Logout":
         logout_section()
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 #     st.markdown("""
 #         <style>
 #             .sidebar .sidebar-content {
